93d2b0d8402a8cf8cac89fd8087026868fbb8d6c
rickyraup <73044129+rickyraup@users.noreply.github.com>
2023-05-28 21:46:41 -0400

    other error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@
 export default function Navbar(){
     return (
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Ricky Raup</a>
+            <a class="navbar-brand" href="https://linkedin.com/in/ricky_raup">Ricky Raup</a>
             <button  type="button">
                 <span class="navbar-toggler-icon"></span>
             </button>
