ae715a31fe7a0850a64e5b5fc853ab4c0e13d1ad
rickyraup <73044129+rickyraup@users.noreply.github.com>
2023-05-30 16:59:27 -0400

    no more scrolling

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,16 +6,16 @@ import linked from "../images/linkedin.png"
 export default function Main(){
     return (
         <div className="main">
-            <div>
+            <div className="my-div">
                 <h3 className="main-text">What's Up!</h3>
-                <h1>I'm Ricky Raup.</h1>
+                <h1 className="main-text3">I'm Ricky Raup.</h1>
                 
                 <img src={pic} className="main-pic" alt=""></img>
                 
 
     
                 <h2 className="main-text2">Comp Sci @UPenn</h2>
-                <h4 className="main-text2">+ Math + Engineering Entrepreneurship + AI</h4>
+                <h4 className="main-text2">+ AI + Engineering Entrepreneurship + Math</h4>
                 <h5 className="main-text2">Interested in Consulting, SWE, and VC.</h5>
                 <div className="links-div">
                     <a target="_blank" rel="noopener noreferrer" href="https://github.com/rickyraup">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@
 
 export default function Navbar(){
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar  navbar-light transparent">
             <a class="navbar-brand" href="https://linkedin.com/in/ricky_raup">Ricky Raup</a>
             <button  type="button">
                 <span class="navbar-toggler-icon"></span>
